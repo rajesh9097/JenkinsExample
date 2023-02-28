b4c7013fb8b0796f7607d1994525e26072893143
rajesh9097 <rajeshsahoo9097@gmail.com>
2023-02-28 20:31:07 +0530

    rest api

--- src/main/java/com/example/JenkinsExample/JenkinsExampleApplication.java
+++ src/main/java/com/example/JenkinsExample/JenkinsExampleApplication.java
@@ -2,12 +2,20 @@ package com.example.JenkinsExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JenkinsExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsExampleApplication.class, args);
+	}
+
+	@GetMapping("/get")
+    public String get(){
+     return "This is a Jenkins Example";
 	}
 
 }
